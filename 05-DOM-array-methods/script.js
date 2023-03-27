c8020bc8e3317d216272bb4956d440f687f9133b
nitesh sah <niteshsah671@gmail.com>
2023-03-28 02:34:11 +0530

    05th project DOM array method. In this project we are implementing the forEach, Sort, reduce, Map and Filter method and perform these opration in a money example. here we are adding random user with random wealth and when a button is click we are running the opropriatre the function and showing the appropriate result

---
+++ 05-DOM-array-methods/script.js
@@ -0,0 +1,97 @@
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const showMillionaires = document.getElementById('show-millionaires');
+const sortBtn = document.getElementById('sort');
+const calculateWealthBtn = document.getElementById('calculate-wealth');
+
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+// Feth random user and add money
+async function getRandomUser() {
+  const res = await fetch('https://randomuser.me/api');
+  const data = await res.json();
+
+  const user = data.results[0];
+
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000),
+  };
+
+  addData(newUser);
+}
+
+// Double the money
+function doubleMoney() {
+  data.map((item) => {
+    item.money = item.money * 2;
+  });
+
+  updateDOM();
+}
+
+// show Millionaires
+function showMillionaire() {
+  data = data.filter((money) => money.money > 1000000);
+
+  updateDOM();
+}
+
+// Calculate the total wealth
+function calculateWealth() {
+  const totalWealth = data.reduce((total, item) => (total += item.money), 0);
+
+  const wealthEl = document.createElement('div');
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(
+    totalWealth
+  )}</strong></h3>`;
+
+  main.appendChild(wealthEl);
+}
+
+// Sort users by richest
+function sortByRichest() {
+  data.sort((a, b) => b.money - a.money);
+
+  updateDOM();
+}
+
+// Add new obj to data arr
+function addData(obj) {
+  data.push(obj);
+
+  updateDOM();
+}
+
+// Update DOM
+function updateDOM(provideData = data) {
+  //clear main div
+  main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+
+  provideData.forEach((item) => {
+    const element = document.createElement('div');
+    element.classList.add('person');
+    element.innerHTML = `<strong>${item.name}</strong> \$${formatMoney(
+      item.money
+    )}`;
+
+    main.appendChild(element);
+  });
+}
+
+// Format number as money
+function formatMoney(number) {
+  return number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+// Event listener
+addUserBtn.addEventListener('click', getRandomUser);
+doubleBtn.addEventListener('click', doubleMoney);
+sortBtn.addEventListener('click', sortByRichest);
+showMillionaires.addEventListener('click', showMillionaire);
+calculateWealthBtn.addEventListener('click', calculateWealth);

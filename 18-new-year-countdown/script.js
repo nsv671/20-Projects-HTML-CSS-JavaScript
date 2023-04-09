274a3e36a5619a09d129f900c2f9201c397ee19c
nitesh sah <niteshsah671@gmail.com>
2023-04-09 13:35:31 +0530

    8th project New Year Countdoen. in this project it will be showing the days, times left to reach new year

---
+++ 18-new-year-countdown/script.js
@@ -0,0 +1,40 @@
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+const countdown = document.getElementById('countdown');
+const year = document.getElementById('year');
+const loading = document.getElementById('loading');
+
+const currentYear = new Date().getFullYear();
+
+const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+
+// Set background year
+year.innerText = currentYear + 1;
+
+// Update countsown time
+function updateCountDown() {
+  const currentTime = new Date();
+  const difference = newYearTime - currentTime;
+
+  const d = Math.floor(difference / 1000 / 60 / 60 / 24);
+  const h = Math.floor(difference / 1000 / 60 / 60) % 24;
+  const m = Math.floor(difference / 1000 / 60) % 60;
+  const s = Math.floor(difference / 1000) % 60;
+
+  //   Add values to DOM
+  days.innerHTML = d;
+  hours.innerHTML = h < 10 ? `0${h}` : h;
+  minutes.innerHTML = m < 10 ? `0${m}` : m;
+  seconds.innerHTML = s < 10 ? `0${s}` : s;
+}
+
+// show spinner before countdown
+setTimeout(() => {
+  countdown.style.display = 'flex';
+  loading.remove();
+}, 1000);
+
+// Run every second
+setInterval(updateCountDown, 1000);
